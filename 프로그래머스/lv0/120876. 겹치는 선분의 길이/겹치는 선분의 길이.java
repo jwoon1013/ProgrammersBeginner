83cc75a3074885e7a77d59b1529a7924d3e6ef5a
woonloveshyandani <117061361+jwoon1013@users.noreply.github.com>
2022-12-13 12:31:44 +0900

    Update 겹치는 선분의 길이.java

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -3,17 +3,17 @@ class Solution {
                //1.계산용 수평선(parallelline) 만들기 -100 ~ 100 -> 0 ~ 200 (모든 수 +100)
         int [] parallelLine = new int[200];
 
-        //2. lines에서 line 하나씩 꺼내. 수평선에 넣어. 그런데 오른쪽 젤끝 수는 +1 안함.
+        //2. lines에서 line 하나씩 꺼내. 수평선에 넣어. 그런데 line의 오른쪽 젤끝 수는 +1 안함.
         for (int j = 0; j < 3 ; j ++){
             int[] line = lines[j]; // 예시 [-1,1]
             for (int i = line[0]; i < line[1]; i++){
-                parallelLine[ i + 100] += 1; // 예시 parallelLine[99],[100]이 1 됨.
+                parallelLine[ i + 100] += 1; // 예시 parallelLine[99],[100]이 +1 됨. [101]은 재낌.
             }
 
         }
 
         //3.parallelLine 원소 중 값이 2, 3인 애 개수 세. >> 이거 아닌듯. 인덱스로 빼야하는듯? ...를 어케함?
-         // 엄청 간단한 방법이 있었음. 2 에서 그냥 [-1,1] 이면 -1, 0 만 +1 하고 오른쪽 끝은 +1 안하는 식으로!
+         // 엄청 간단한 방법이 있었음. 2 에서 그냥 [-1,1] 이면[99], 0 만 +1 하고 오른쪽 끝은 +1 안하는 식으로!
         int answer = 0;
         for(int i = 0 ; i < parallelLine.length; i++){
             if(parallelLine[i] > 1){
